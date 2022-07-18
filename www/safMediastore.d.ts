6823274f0f895ef03941c6813d3e1db7c716efda
Wilson Foo Yu Kang <wilson.foo@customautosys.com>
2022-07-18 13:47:29 +0800

    Types were not working properly.

--- www/safMediastore.d.ts
+++ www/safMediastore.d.ts
@@ -1,20 +1,22 @@
+export default interface SafMediastore{
+	selectFolder(uri?:string):Promise<string>,
+	selectFile(uri?:string):Promise<string>,
+	openFolder(uri:string):Promise<void>,
+	openFile(uri:string):Promise<void>,
+	readFile(uri:string):Promise<ArrayBuffer>,
+	writeFile(params:{
+		data:ArrayBuffer,
+		filename:string,
+		folder?:string,
+		subfolder?:string
+	}):Promise<string>,
+	saveFile(params:{
+		data:ArrayBuffer,
+		filename?:string,
+		folder?:string
+	}):Promise<string>
+};
+
 interface CordovaPlugins{
-	safMediastore:{
-		selectFolder(uri?:string):Promise<string>,
-		selectFile(uri?:string):Promise<string>,
-		openFolder(uri:string):Promise<void>,
-		openFile(uri:string):Promise<void>,
-		readFile(uri:string):Promise<ArrayBuffer>,
-		writeFile(params:{
-			data:ArrayBuffer,
-			filename:string,
-			folder?:string,
-			subfolder?:string
-		}):Promise<string>,
-		saveFile(params:{
-			data:ArrayBuffer,
-			filename?:string,
-			folder?:string
-		}):Promise<string>
-	}
-}
+	safMediastore:SafMediastore
+};
