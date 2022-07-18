a2d28163b23e285f74440a1ad543187dda3d377f
Wilson Foo Yu Kang <wilson.foo@customautosys.com>
2022-07-18 13:38:30 +0800

    Changed plugin clobbering.

--- www/safMediastore.d.ts
+++ www/safMediastore.d.ts
@@ -1,28 +1,24 @@
 export {};
 
 declare global{
-    interface SafMediastore{
-        selectFolder(uri?:string):Promise<string>,
-        selectFile(uri?:string):Promise<string>,
-        openFolder(uri:string):Promise<void>,
-        openFile(uri:string):Promise<void>,
-        readFile(uri:string):Promise<ArrayBuffer>,
-        writeFile(params:{
-            data:ArrayBuffer,
-            filename:string,
-            folder?:string,
-            subfolder?:string
-        }):Promise<string>,
-        saveFile(params:{
-            data:ArrayBuffer,
-            filename?:string,
-            folder?:string
-        }):Promise<string>
-    }
-
-    interface Window{
-        safMediastore:SafMediastore;
-    }
-
-    export const safMediastore:SafMediastore;
+	interface CordovaPlugins{
+		safMediastore:{
+			selectFolder(uri?:string):Promise<string>,
+			selectFile(uri?:string):Promise<string>,
+			openFolder(uri:string):Promise<void>,
+			openFile(uri:string):Promise<void>,
+			readFile(uri:string):Promise<ArrayBuffer>,
+			writeFile(params:{
+				data:ArrayBuffer,
+				filename:string,
+				folder?:string,
+				subfolder?:string
+			}):Promise<string>,
+			saveFile(params:{
+				data:ArrayBuffer,
+				filename?:string,
+				folder?:string
+			}):Promise<string>
+		};
+	}
 }
