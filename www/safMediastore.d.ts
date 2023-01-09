eca7ba2c427ab9d24e74778e9cd07ffe38819c22
Massimiliano Vinciprova <massimiliano.vinciprova@gmail.com>
2023-01-09 18:17:51 +0100

    adding type for the new functions

--- www/safMediastore.d.ts
+++ www/safMediastore.d.ts
@@ -1,33 +1,36 @@
-interface SafMediastore{
-	selectFolder(uri?:string):Promise<string>,
-	selectFile(uri?:string):Promise<string>,
-	openFolder(uri:string):Promise<void>,
-	openFile(uri:string):Promise<void>,
-	readFile(uri:string):Promise<ArrayBuffer>,
-	writeFile(params:{
-		data:string,
-		filename:string,
-		folder?:string,
-		subFolder?:string
-	}):Promise<string>,
-	overwriteFile(params:{
-		uri:string,
-		data:string
-	}):Promise<string>,
-	saveFile(params:{
-		data:string,
-		filename?:string,
-		folder?:string
-	}):Promise<string>,
-	deleteFile(uri:string):Promise<number>,
-	getFileName(uri:string):Promise<string>,
-	getUri(params:{
-		folder:string,
-		subFolder?:string,
-		filename?:string,
-	}):Promise<string>
+interface SafMediastore {
+  selectFolder(uri?: string): Promise<string>;
+  selectFile(uri?: string): Promise<string>;
+  openFolder(uri: string): Promise<void>;
+  openFile(uri: string): Promise<void>;
+  readFile(uri: string): Promise<ArrayBuffer>;
+  writeFile(params: {
+    data: string;
+    filename: string;
+    folder?: string;
+    subFolder?: string;
+  }): Promise<string>;
+  overwriteFile(params: { uri: string; data: string }): Promise<string>;
+  saveFile(params: {
+    data: string;
+    filename?: string;
+    folder?: string;
+  }): Promise<string>;
+  deleteFile(uri: string): Promise<number>;
+  getFileName(uri: string): Promise<string>;
+  getUri(params: {
+    folder: string;
+    subFolder?: string;
+    filename?: string;
+  }): Promise<string>;
+  existsFile(filename: string): Promise<string>;
+  getDataForOpenFile(filename: string): Promise<{
+    uri: string;
+    mimeType: string;
+    filename: string;
+  }>;
 }
 
-interface CordovaPlugins{
-	safMediastore:SafMediastore
+interface CordovaPlugins {
+  safMediastore: SafMediastore;
 }
